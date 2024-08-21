8495166ff16b1886f223bdb03a54350d48fdaad7
Jonas Nutels <jonas.nutels@tceal.tc.br>
2024-08-20 21:03:01 -0300

    ajuste madson

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,21 @@
+import styles from './Footer.module.css';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className={styles.container}>
+      <div className={styles.buttons}>
+        <h5>
+          <LocalPhoneIcon /> Telefone
+        </h5>
+        <h5>
+          <InstagramIcon /> Instagram
+        </h5>
+        <h5>
+          <WhatsAppIcon /> WhatsApp
+        </h5>
+      </div>
     </div>
   );
 }
